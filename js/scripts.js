4619259439053ccf66c7e9276dba3617aa7e433e
Allan <allanjheyson@hotmail.com>
2022-09-04 08:09:02 -0300

    Testes

--- js/scripts.js
+++ js/scripts.js
@@ -7,16 +7,53 @@ var x = frase.split("\n");
 document.getElementById("frase").innerHTML = x[0];
 document.getElementById("autor").innerHTML = x[1];
 
-document.addEventListener("DOMContentLoaded", function() {
+
+function novaFrase() {
+   location.reload();
+}
+
+
+function takeshot() {
+    html2canvas(document.querySelector(".card")).then(canvas => {
+        document.getElementById("output").appendChild(canvas)
+    });
+}
+
+
+function toggleFullScreen() {
+    if (!document.fullscreenElement &&    // alternative standard method
+        !document.mozFullScreenElement && !document.webkitFullscreenElement && !document.msFullscreenElement) {  // current working methods
+        if (document.documentElement.requestFullscreen) {
+            document.documentElement.requestFullscreen();
+        } else if (document.documentElement.msRequestFullscreen) {
+            document.documentElement.msRequestFullscreen();
+        } else if (document.documentElement.mozRequestFullScreen) {
+            document.documentElement.mozRequestFullScreen();
+        } else if (document.documentElement.webkitRequestFullscreen) {
+            document.documentElement.webkitRequestFullscreen(Element.ALLOW_KEYBOARD_INPUT);
+        }
+    } else {
+        if (document.exitFullscreen) {
+            document.exitFullscreen();
+        } else if (document.msExitFullscreen) {
+            document.msExitFullscreen();
+        } else if (document.mozCancelFullScreen) {
+            document.mozCancelFullScreen();
+        } else if (document.webkitExitFullscreen) {
+            document.webkitExitFullscreen();
+        }
+    }
+}
+
+const btn = document.querySelector("#btnprint");
+
+btn.addEventListener("click", function () {
+    toggleFullScreen();
+});
+
+document.addEventListener("DOMContentLoaded", function () {
     //conteúdo que será compartilhado: Título da página + URL
     var conteudo = "*" + x[0] + "* " + x[1];
     //altera a URL do botão
     document.getElementById("whatsapp-share-btt").href = "https://api.whatsapp.com/send?text=" + conteudo;
 }, false);
-
-
-function takeshot(){
-    html2canvas(document.querySelector(".card")).then(canvas => {
-        document.getElementById("output").appendChild(canvas)
-    });
-}
