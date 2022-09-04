90f0dee51b0d043c6ada6011de0509e5dd835cc1
Allan <allanjheyson@hotmail.com>
2022-09-04 06:03:36 -0300

    Teste whatsapp

--- js/scripts.js
+++ js/scripts.js
@@ -7,6 +7,13 @@ var x = frase.split("\n");
 document.getElementById("frase").innerHTML = x[0];
 document.getElementById("autor").innerHTML = x[1];
 
+document.addEventListener("DOMContentLoaded", function() {
+    //conteúdo que será compartilhado: Título da página + URL
+    var conteudo = "*" + x[0] + "* " + x[1];
+    //altera a URL do botão
+    document.getElementById("whatsapp-share-btt").href = "https://api.whatsapp.com/send?text=" + conteudo;
+}, false);
+
 
 function takeshot(){
     html2canvas(document.querySelector(".card")).then(canvas => {
